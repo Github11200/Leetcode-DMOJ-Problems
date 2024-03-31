e2d98dc3bef3b526a96d2ec57c4be1d1a70769a5
Github11200 <78944617+Github11200@users.noreply.github.com>
2024-03-30 23:07:51 -0700

    solved some problems in C++ and also did another leetcode content

---
+++ LeetcodeContests/Weekly-Contest-391.cpp
@@ -0,0 +1,91 @@
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int harshadNumber(int x)
+{
+    if (x == 100)
+        return 1;
+    return x % (x % 10 + int(x / 10)) == 0 ? x % 10 + int(x / 10) : -1;
+}
+
+int waterBottlesTwo(int numBottles, int numExchange)
+{
+    int fullBottles = numBottles;
+    int emptyBottles = 0;
+    int numExchangeLocal = numExchange;
+    int bottlesDrunk = 0;
+
+    while (true)
+    {
+        if (emptyBottles >= numExchangeLocal)
+        {
+            fullBottles += 1;
+            emptyBottles -= numExchangeLocal;
+            numExchangeLocal += 1;
+        }
+        else if (fullBottles != 0)
+        {
+            bottlesDrunk += fullBottles;
+            emptyBottles += fullBottles;
+            fullBottles = 0;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return bottlesDrunk;
+}
+
+bool isAlternating(vector<int> nums)
+{
+    for (int i = 1; i < size(nums); ++i)
+    {
+        if (nums[i] == nums[i - 1])
+            return false;
+    }
+
+    return true;
+}
+
+int additionFactorial(int x)
+{
+    int sum = 0;
+    for (int i = 1; i <= x; ++i)
+        sum += i;
+    return sum;
+}
+
+long long countAlternatingSubarrays(vector<int> &nums)
+{
+    long long int numberOfAlternatingSubarrays = nums.size();
+    int j = 1;
+
+    for (int i = 1; i < nums.size(); ++i)
+    {
+        if (i == nums.size() || nums[i] != nums[i - 1])
+        {
+            numberOfAlternatingSubarrays += j;
+            j += 1;
+        }
+        else
+            j = 1;
+    }
+
+    return numberOfAlternatingSubarrays;
+}
+
+int main()
+{
+    vector<int> test({1, 0, 1, 0});
+
+    cout << countAlternatingSubarrays(test) << endl;
+
+    return 0;
+}
--- problems.cpp
+++ problems.cpp
@@ -1,10 +1,73 @@
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <queue>
 #include <iostream>
 
 using namespace std;
 
+// https://leetcode.com/problems/contains-duplicate/
+bool containsDuplicate(vector<int> &nums)
+{
+    unordered_map<int, int> duplicates;
+
+    for (auto x : nums)
+    {
+        if (duplicates.find(x) != duplicates.end())
+            return true;
+        else
+            duplicates[x] = 0;
+    }
+
+    return false;
+}
+
+// https://leetcode.com/problems/concatenation-of-array/description/
+vector<int> concatenationOfArray(vector<int> &nums)
+{
+    vector<int> results;
+    results.reserve(size(nums) * 2);
+    results.insert(results.end(), nums.begin(), nums.end());
+    results.insert(results.end(), nums.begin(), nums.end());
+    return results;
+}
+
+// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/submissions/1218706537/
+vector<int> replaceAlementsWithGreatestElementOnRightSide(vector<int> &arr)
+{
+    vector<int> outputArray(size(arr));
+    int previousValue = -1;
+    outputArray[size(arr) - 1] = -1;
+
+    for (int i = size(arr) - 1; i > 0; --i)
+    {
+        if (arr[i] > previousValue)
+        {
+            outputArray[i - 1] = arr[i];
+            previousValue = arr[i];
+        }
+        else
+            outputArray[i - 1] = previousValue;
+    }
+
+    return outputArray;
+}
+
+bool isSubsequence(string s, string t)
+{
+}
+
+template <typename T>
+void displayVector(vector<T> arr)
+{
+    for (auto x : arr)
+        cout << x << endl;
+}
+
 int main()
 {
-    cout << "Hello World" << endl;
-
+    vector<int> inputData({17, 18, 5, 4, 6, 1});
+    // displayVector(replaceAlementsWithGreatestElementOnRightSide(inputData));
+    cout << isSubsequence("acb", "ahbgdc") << endl;
     return 0;
 }
