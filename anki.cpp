a83088f1d5e629eb5284515e23e30994c6ce069c
Github11200 <pateljinay@outlook.com>
2024-06-12 18:56:43 -0700

    have one more anki problem left

--- anki.cpp
+++ anki.cpp
@@ -26,40 +26,12 @@ void display2DVector(vector<vector<T> > arr) {
     }
 }
 
-vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2) {
-    unordered_map<int, int> nums1Hash;
-    for (int i = 0; i < nums1.size(); ++i)
-        nums1Hash[nums1[i]] = i;
-
-    for (int i = 0; i < nums2.size(); ++i) {
-        if (nums1Hash.count(nums2[i]) > 0) {
-            stack<int> largerNums;
-            largerNums.push(nums2[i]);
-
-            int j = i + 1;
-            for (; j < nums2.size(); ++j) {
-                if (nums2[j] > nums2[i])
-                    break;
-                if (nums1Hash.count(nums2[j]) > 0)
-                    largerNums.push(nums2[j]);
-            }
-
-            while (!largerNums.empty()) {
-                int item = largerNums.top();
-                largerNums.pop();
-                nums1[nums1Hash[item]] = j < nums2.size() ? nums2[j] : -1;
-            }
-        }
-    }
-
-    return nums1;
-}
 
 int main() {
-    vector<int> nums1 = {2, 4};
-    vector<int> nums2 = {1, 2, 3, 4};
+    vector<int> nums1 = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    vector<int> nums2 = {3, 2, 3};
 
-    displayVector(nextGreaterElement(nums1, nums2));
+    cout << containerWithMostWater(nums1) << endl;
 
     return 0;
 }
