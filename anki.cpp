ed41ec74eea84403ce933964ced53f8a37a9b473
Github11200 <pateljinay@outlook.com>
2024-10-14 15:29:59 -0700

    did some more algorithms from the flashcards

--- anki.cpp
+++ anki.cpp
@@ -38,19 +38,96 @@ vector<int> visited(N + 1);
 vector<tuple<int, int, int>> edgeListRepresentation(N + 1);
 vector<vector<int>> adjacencymatrixRepresentation;
 
+void bfs(int x)
+{
+  queue<int> nextNodes;
+  vector<int> distances(N + 1, INFINITY);
+
+  nextNodes.push(x);
+  distances[x] = 0;
+  while (!nextNodes.empty())
+  {
+    int currentNode = nextNodes.front();
+    nextNodes.pop();
+    if (visited[currentNode])
+      continue;
+    visited[currentNode] = true;
+
+    for (auto u : adj[currentNode])
+    {
+      int b = u.first;
+      distances[b] = distances[currentNode] + 1;
+      nextNodes.push(b);
+    }
+  }
+}
+
+struct UnionFind
+{
+  vector<int> parents;
+  vector<int> sizes;
+
+  void init(int n)
+  {
+    for (int i = 0; i < n; ++i)
+    {
+      this->parents[i] = i;
+      this->sizes[i] = 1;
+    }
+  }
+
+  int find(int x)
+  {
+    while (x != this->parents[x])
+      x = this->parents[x];
+    return x;
+  }
+
+  bool same(int a, int b)
+  {
+    return this->find(a) == this->find(b);
+  }
+
+  void unite(int a, int b)
+  {
+    a = this->find(a);
+    b = this->find(b);
+    if (this->sizes[a] < this->sizes[b])
+      swap(a, b);
+    this->sizes[a] += this->sizes[b];
+    this->parents[b] = a;
+  }
+};
+
+void bellmanFord(int x)
+{
+  vector<int> distances(N + 1, INFINITY);
+
+  for (int i = 0; i < N; ++i)
+  {
+    for (auto e : edgeListRepresentation)
+    {
+      int a, b, w;
+      tie(a, b, w) = e;
+      distances[b] = min(distances[b], distances[a] + w);
+    }
+  }
+}
+
 void dijkstras(int x)
 {
   vector<int> distances(N + 1, INFINITY);
   priority_queue<pair<int, int>> nextNodes;
 
-  distances[x] = 0;
   nextNodes.push({0, x});
   while (!nextNodes.empty())
   {
     int currentNode = nextNodes.top().second;
+    nextNodes.pop();
     if (visited[currentNode])
       continue;
     visited[currentNode] = true;
+
     for (auto u : adj[currentNode])
     {
       int b = u.first;
@@ -62,6 +139,16 @@ void dijkstras(int x)
       }
     }
   }
+}
+
+bool isPrime(int x)
+{
+  for (int i = 1; i * i < x; ++i)
+  {
+    if (x % i == 0)
+      return false;
+  }
+  return true;
 }
 
 int main()
