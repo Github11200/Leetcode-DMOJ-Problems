a5dc5a2d2f24254934dddcf81a9819bef3cd7df3
Github11200 <pateljinay@outlook.com>
2024-10-16 18:38:02 -0700

    did recursive DFS and a function to get the factors

--- anki.cpp
+++ anki.cpp
@@ -38,127 +38,43 @@ vector<int> visited(N + 1);
 vector<tuple<int, int, int>> edgeListRepresentation(N + 1);
 vector<vector<int>> adjacencymatrixRepresentation;
 
-void bfs(int x)
+vector<int> getFactors(int x)
 {
-  queue<int> nextNodes;
-  vector<int> distances(N + 1, INFINITY);
+  vector<int> factors;
+  for (int i = 2; i * i < x; ++i)
+  {
+    while (x % i == 0)
+    {
+      factors.push_back(i);
+      x /= i;
+    }
+  }
+  if (x > 1)
+    factors.push_back(1);
+  return factors;
+}
+
+void recursiveDfs(int x)
+{
+  stack<int> nextNodes;
 
   nextNodes.push(x);
-  distances[x] = 0;
   while (!nextNodes.empty())
   {
-    int currentNode = nextNodes.front();
+    int currentNode = nextNodes.top();
     nextNodes.pop();
     if (visited[currentNode])
       continue;
     visited[currentNode] = true;
-
     for (auto u : adj[currentNode])
     {
-      int b = u.first;
-      distances[b] = distances[currentNode] + 1;
-      nextNodes.push(b);
+      nextNodes.push(u.first);
     }
   }
-}
-
-struct UnionFind
-{
-  vector<int> parents;
-  vector<int> sizes;
-
-  void init(int n)
-  {
-    for (int i = 0; i < n; ++i)
-    {
-      this->parents[i] = i;
-      this->sizes[i] = 1;
-    }
-  }
-
-  int find(int x)
-  {
-    while (x != this->parents[x])
-      x = this->parents[x];
-    return x;
-  }
-
-  bool same(int a, int b)
-  {
-    return this->find(a) == this->find(b);
-  }
-
-  void unite(int a, int b)
-  {
-    a = this->find(a);
-    b = this->find(b);
-    if (this->sizes[a] < this->sizes[b])
-      swap(a, b);
-    this->sizes[a] += this->sizes[b];
-    this->parents[b] = a;
-  }
-};
-
-void bellmanFord(int x)
-{
-  vector<int> distances(N + 1, INFINITY);
-
-  for (int i = 0; i < N; ++i)
-  {
-    for (auto e : edgeListRepresentation)
-    {
-      int a, b, w;
-      tie(a, b, w) = e;
-      distances[b] = min(distances[b], distances[a] + w);
-    }
-  }
-}
-
-void dijkstras(int x)
-{
-  vector<int> distances(N + 1, INFINITY);
-  priority_queue<pair<int, int>> nextNodes;
-
-  nextNodes.push({0, x});
-  while (!nextNodes.empty())
-  {
-    int currentNode = nextNodes.top().second;
-    nextNodes.pop();
-    if (visited[currentNode])
-      continue;
-    visited[currentNode] = true;
-
-    for (auto u : adj[currentNode])
-    {
-      int b = u.first;
-      int w = u.second;
-      if (distances[currentNode] + w < distances[b])
-      {
-        distances[b] = distances[currentNode] + w;
-        nextNodes.push({-distances[b], b});
-      }
-    }
-  }
-}
-
-bool isPrime(int x)
-{
-  for (int i = 1; i * i < x; ++i)
-  {
-    if (x % i == 0)
-      return false;
-  }
-  return true;
 }
 
 int main()
 {
-  // .first = b, and .second = w
-  adj[9] = {{7, 3}, {1, 8}};
-  adj[7] = {{1, 6}, {3, 1}};
-  adj[3] = {{5, 4}};
-  adj[1] = {{5, 2}};
-
   edgeListRepresentation.push_back(make_tuple(9, 7, 3));
   edgeListRepresentation.push_back(make_tuple(9, 1, 8));
   edgeListRepresentation.push_back(make_tuple(7, 1, -6));
