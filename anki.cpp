2c3a8f3abf6acab5334c0253dab80f6cbe51eac3
Github11200 <pateljinay@outlook.com>
2025-01-20 21:44:59 -0800

    did some flashcards and was working on problem

--- anki.cpp
+++ anki.cpp
@@ -2,117 +2,89 @@
 
 using namespace std;
 
-string addBinary(string a, string b)
+void iterativeInOrderTraversal(TreeNode *root)
 {
-  int i = a.size() - 1;
-  int j = b.size() - 1;
-  int carry = 0;
-  string c = "";
-  while (i >= 0 || j >= 0 || carry > 0)
+  stack<TreeNode *> nextNodes;
+  while (!nextNodes.empty() || root != nullptr)
   {
-    if (i >= 0 && a[i] == '1')
-      ++carry;
-    if (j >= 0 && b[j] == '1')
-      ++carry;
-
-    c = to_string(carry % 2) + c;
-    carry /= 2;
-    --i;
-    --j;
+    while (root != nullptr)
+    {
+      nextNodes.push(root);
+      root = root->left;
+    }
+    root = nextNodes.top();
+    nextNodes.pop();
+    printf("%d ", root->val);
+    root = root->right;
   }
-
-  return c;
 }
 
-void gates()
+void iterativePreOrderTraversal(TreeNode *root)
 {
-  int numberOfGates, numberOfPlanes;
-  scanf("%d", &numberOfGates);
-  scanf("%d", &numberOfPlanes);
-
-  set<int, greater<int>> gates;
-  vector<int> planes(numberOfPlanes);
-
-  for (int i = 0; i < numberOfPlanes; ++i)
-    scanf("%d", &planes[i]);
-
-  for (int i = 1; i <= numberOfGates; ++i)
-    gates.insert(i);
-
-  int planesDocked = 0;
-  for (int i = 0; i < numberOfPlanes; ++i)
+  stack<TreeNode *> nextNodes;
+  nextNodes.push(root);
+  while (!nextNodes.empty())
   {
-    auto result = gates.lower_bound(planes[i]);
-    if (result == gates.end())
-      break;
-    ++planesDocked;
-    gates.erase(*result);
+    root = nextNodes.top();
+    nextNodes.pop();
+    printf("%d ", root->val);
+    if (root->right != nullptr)
+      nextNodes.push(root->right);
+    if (root->left != nullptr)
+      nextNodes.push(root->left);
   }
-
-  printf("%d\n", planesDocked);
 }
 
-void swipe()
+void iterativePostOrderTraversal(TreeNode *root)
 {
-  int N;
-  scanf("%d", &N);
-
-  vector<int> A(N), B(N), BPrime, firstOccurences, lastOccurences;
-  for (int i = 0; i < N; ++i)
-    scanf("%d", &A[i]);
-
-  for (int i = 0; i < N; ++i)
+  stack<TreeNode *> nextNodes;
+  while (!nextNodes.empty() || root != nullptr)
   {
-    scanf("%d", &B[i]);
-    if (BPrime.size() == 0 || BPrime[BPrime.size() - 1] != B[i])
+    if (root != nullptr)
     {
-      firstOccurences.push_back(i);
-      lastOccurences.push_back(i);
-      BPrime.push_back(B[i]);
+      nextNodes.push(root);
+      root = root->left;
     }
-    else if (BPrime[BPrime.size() - 1] == B[i])
-      ++lastOccurences[lastOccurences.size() - 1];
-  }
-
-  int j = 0;
-  for (int i = 0; i < A.size(); ++i)
-    if (A[i] == BPrime[j])
-      ++j;
-
-  if (j == BPrime.size())
-    printf("YES\n");
-  else
-  {
-    printf("NO\n");
-    return;
-  }
-
-  j = 0;
-  vector<pair<int, int>> leftSwipes, rightSwipes;
-  for (int i = 0; i < A.size() && j < BPrime.size(); ++i)
-  {
-    if (A[i] == BPrime[j])
+    else
     {
-      if (i < lastOccurences[j])
-        rightSwipes.push_back(pair<int, int>(i, lastOccurences[j]));
-      if (i > firstOccurences[j])
-        leftSwipes.push_back(pair<int, int>(firstOccurences[j], i));
-      ++j;
+      TreeNode *temp = nextNodes.top()->right;
+      if (temp == nullptr)
+      {
+        temp = nextNodes.top();
+        nextNodes.pop();
+        printf("%d ", temp->val);
+        while (!nextNodes.empty() && temp == nextNodes.top()->right)
+        {
+          temp = nextNodes.top();
+          nextNodes.pop();
+          printf("%d ", temp->val);
+        }
+      }
+      else
+        root = temp;
     }
   }
+}
 
-  sort(rightSwipes.rbegin(), rightSwipes.rend());
-  sort(leftSwipes.begin(), leftSwipes.end());
-
-  printf("%d\n", rightSwipes.size() + leftSwipes.size());
-  for (int i = 0; i < rightSwipes.size(); ++i)
-    printf("R %d %d\n", rightSwipes[i].first, rightSwipes[i].second);
-  for (int i = 0; i < leftSwipes.size(); ++i)
-    printf("L %d %d\n", leftSwipes[i].first, leftSwipes[i].second);
+int jumpGame2(vector<int> nums)
+{
+  int l = 0;
+  int r = 0;
+  int res = 0;
+  while (r < nums.size() - 1)
+  {
+    int farthest = 0;
+    for (int i = l; i <= r; ++i)
+      farthest = max(farthest, nums[i] + i);
+    l = r + 1;
+    r = farthest;
+    ++res;
+  }
+  return res;
 }
 
 int main()
 {
-  swipe();
+  cout << jumpGame2(vector<int>({2, 3, 1, 1, 4})) << endl;
   return 0;
 }
