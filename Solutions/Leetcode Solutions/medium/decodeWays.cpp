2baff9843da1cf48700e1cf947add5a9d06034c0
Github11200 <pateljinay@outlook.com>
2025-02-02 22:26:09 -0800

    solved the string problem from yesterday

--- Solutions/Leetcode Solutions/medium/decodeWays.cpp
+++ Solutions/Leetcode Solutions/medium/decodeWays.cpp
@@ -3,21 +3,34 @@
 using namespace std;
 
 /******************************************
-  Link:
-  Runtime:
-  Memory:
+  Link: https://leetcode.com/problems/decode-ways/description/
+  Runtime: 100.00%
+  Memory: 90.44%
 ******************************************/
 
 int decodeWays(string s)
 {
-  int numberOfWays = 0;
-  for (int i = 0; i < s.size(); ++i)
+  int temp = 1;
+  int current = 1;
+  int next = 1;
+  for (int i = s.size() - 1; i >= 0; --i)
   {
+    if (s[i] == '0')
+    {
+      current = 0;
+      continue;
+    }
+    temp = current;
+    if (i + 1 < s.size() && (s[i] == '1' || (s[i] == '2' && int(s[i + 1]) <= 54)))
+      temp += next;
+    next = current;
+    current = temp;
   }
+  return temp;
 }
 
 int main()
 {
-  cout << decodeWays("/../..ga/b/.f..d/..../e.baaeeh./.a") << endl;
+  cout << decodeWays("226") << endl;
   return 0;
 }
