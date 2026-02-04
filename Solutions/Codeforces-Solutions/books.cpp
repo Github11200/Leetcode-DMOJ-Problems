3e5a7b9954440207387ed8de7fcae5499141a70a
Github11200 <pateljinay@outlook.com>
2026-02-03 22:30:54 -0800

    done another problem

---
+++ Solutions/Codeforces-Solutions/books.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+  int n, freeMinutes;
+  cin >> n >> freeMinutes;
+
+  vector<int> bookTimes(n);
+  for (auto &bookTime : bookTimes)
+    cin >> bookTime;
+
+  int i = 0;
+  int j = 0;
+
+  int sum = 0;
+  int maxNumberOfBooksRead = 0;
+  for (; j < bookTimes.size(); ++j) {
+    if (bookTimes[j] + sum > freeMinutes) {
+      int booksRead = j - i;
+      if (booksRead > maxNumberOfBooksRead)
+        maxNumberOfBooksRead = booksRead;
+      sum -= bookTimes[i++];
+    }
+    sum += bookTimes[j];
+  }
+
+  if (sum <= freeMinutes && j - i > maxNumberOfBooksRead)
+    maxNumberOfBooksRead = j - i;
+  cout << maxNumberOfBooksRead << endl;
+
+  return 0;
+}
